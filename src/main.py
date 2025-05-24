439cbeed256a1f74a378141066b542b08d385d33
calm1205 <nagi.moriyama@mntsq.com>
2025-05-24 22:06:40 +0900

    feat: set index.html

--- src/main.py
+++ src/main.py
@@ -1,19 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def read_root():
-    html_content = """
-    <html>
-      <body>
-        <h1>Hello World</h1>
-      </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content)
+def read_root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.get("/health")
 def health():
