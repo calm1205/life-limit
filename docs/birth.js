adf71e38725d84aa1f218275c98daac06f50f28a
calm1205 <nagi.moriyama@mntsq.com>
2025-06-07 18:13:14 +0900

    feat: rest day

--- docs/birth.js
+++ docs/birth.js
@@ -22,6 +22,16 @@ export function calculateMonths(year, month, day) {
   return years * 12 + months
 }
 
+export function calculateDays(year, month, day) {
+  const birthDate = new Date(year, month - 1, day)
+  const today = new Date()
+
+  const diffTime = Math.abs(today - birthDate)
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+
+  return diffDays
+}
+
 export function getBirthParams() {
   const urlParams = new URLSearchParams(window.location.search)
   const year = parseInt(urlParams.get("year"))
